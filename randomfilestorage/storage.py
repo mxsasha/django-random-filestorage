4ae4b712169a16784317208a26f7605a87b294f9
Gabriel - Iulian Dumbrava <gabriel.dumbrava@gmail.com>
2019-05-22 16:39:42 +0300

    Fix missing max_length. Still not used ...

--- randomfilestorage/storage.py
+++ randomfilestorage/storage.py
@@ -6,13 +6,14 @@ from django.core.files.storage import FileSystemStorage
 
 RANDOM_FILENAME_LENGTH = 60
 
+
 class RandomFileSystemStorage(FileSystemStorage):
 
     def get_valid_name(self, name):
         file_root, file_ext = os.path.splitext(name)
         return "%s%s" % (get_random_string(RANDOM_FILENAME_LENGTH).lower(), file_ext)
 
-    def get_available_name(self, name):
+    def get_available_name(self, name, max_length=None):
         dir_name, file_name = os.path.split(name)
         file_root, file_ext = os.path.splitext(file_name)
         while self.exists(name):
